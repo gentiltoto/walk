8d070ddf357695bfa3bddf4e132f922a5b77403a
cassandre jacson <cassandre.jacson@edhec.com>
2018-10-29 13:46:15 +0100

    fini

--- app/javascript/components/grandefichemonument.js
+++ app/javascript/components/grandefichemonument.js
@@ -9,3 +9,17 @@
 // }
 
 // export { flip }
+
+function flip() {
+  const monument = document.getElementById("flip-card-front");
+  const map = document.getElementById("flip-card-back");
+  const flip = document.getElementById("flip-card");
+
+  flip.addEventListener('click', function() {
+    flip.classList.toggle('do-flip');
+
+    console.log("hello")
+  })
+}
+
+export { flip }
