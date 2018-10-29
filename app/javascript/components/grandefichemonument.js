dde3255aa780d583f070d119f376e9f8691a5d36
cassandre jacson <cassandre.jacson@edhec.com>
2018-10-29 11:13:06 +0100

    last modif

--- app/javascript/components/grandefichemonument.js
+++ app/javascript/components/grandefichemonument.js
@@ -1,11 +1,11 @@
-function flip() {
-  const card = document.getElementById('flip-card');
-  const flipBtn = document.querySelector('#flip-card-button');
+// function flip() {
+//   const card = document.getElementById('flip-card');
+//   const flipBtn = document.querySelector('#flip-card-button');
 
-  document.getElementById('flip-card-button').addEventListener('click', function() {
-    card.classList.toggle('do-flip');
-    console.log("hello")
-  }, false);
-}
+//   document.getElementById('flip-card-button').addEventListener('click', function() {
+//     card.classList.toggle('do-flip');
+//     console.log("hello")
+//   }, false);
+// }
 
-export { flip }
+// export { flip }
