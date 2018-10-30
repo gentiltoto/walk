ad10059c9c0d13716d0da0346d398b60b1e52ac1
Cassandre Jacson <42711656+cassandrejacson@users.noreply.github.com>
2018-10-30 11:54:51 +0100

    Revert "Grande fiche monument"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,8 @@
 import "bootstrap";
-
-
-import { flip } from '../components/grandefichemonument.js';
-// set the flip of the grande fiche monument
-flip();
-
 import { openNav, closeNav } from "./navbar.js"
 
 
 // Set Listeners for the Mobile Navbar
 openNav();
 closeNav();
-
 
