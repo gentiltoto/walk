b510f75e7b1ac2ec9ce8477319e37ca9d4875eca
AntoineTorrini <antoine.torrini@gmail.com>
2018-10-30 17:10:20 +0100

    js repaired

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 import "bootstrap";
 
 
-import { flip } from '../components/grandefichemonument.js';
-// set the flip of the grande fiche monument
-flip();
+// import { flip } from '../components/grandefichemonument.js';
+// // set the flip of the grande fiche monument
+// flip();
 
 import { openNav, closeNav } from "./navbar.js"
 
@@ -11,5 +11,3 @@ import { openNav, closeNav } from "./navbar.js"
 // Set Listeners for the Mobile Navbar
 openNav();
 closeNav();
-
-
---
+++ app/javascript/packs/grandefichemonument.js
@@ -0,0 +1,3 @@
+import { flip } from "../components/grandefichemonument.js"
+
+flip();
