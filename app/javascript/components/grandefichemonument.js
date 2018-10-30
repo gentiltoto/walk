0795788dc0834da2cb3eaf769f2836c6387b9443
cassandre jacson <cassandre.jacson@edhec.com>
2018-10-30 11:14:29 +0100

    fiche à jour avec rails ujs

--- app/javascript/components/grandefichemonument.js
+++ app/javascript/components/grandefichemonument.js
@@ -1,15 +1,3 @@
-// function flip() {
-//   const card = document.getElementById('flip-card');
-//   const flipBtn = document.querySelector('#flip-card-button');
-
-//   document.getElementById('flip-card-button').addEventListener('click', function() {
-//     card.classList.toggle('do-flip');
-//     console.log("hello")
-//   }, false);
-// }
-
-// export { flip }
-
 function flip() {
   const monument = document.getElementById("flip-card-front");
   const map = document.getElementById("flip-card-back");
@@ -18,7 +6,6 @@ function flip() {
   flip.addEventListener('click', function() {
     flip.classList.toggle('do-flip');
 
-    console.log("hello")
   })
 }
 
