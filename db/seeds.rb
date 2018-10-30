da7a3fb500a925e070f1a6bc81e614ebf43c656f
AntoineTorrini <antoine.torrini@gmail.com>
2018-10-30 16:25:23 +0100

    on cascade: delete & new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@ rihour.save
 grand_place = Monument.new(name: "Grand Place", address: "Grand Place", latitude: "0.00", longitude: "0.00", description: "Une joli description de grand place", score: 10000)
 grand_place.city = lille
 grand_place.save
+
+beaux_arts = Monument.new(name: "Musée des Beaux Arts", address: "Répu", latitude: "0.00", longitude: "0.00", description: "Une joli description des Beaux Arts", score: 10000)
+beaux_arts.city = lille
+beaux_arts.save
