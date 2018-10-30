aefc8baef8b1a2e6da5a4a6ffbb1d2e174ffef8b
AntoineTorrini <antoine.torrini@gmail.com>
2018-10-30 11:32:58 +0100

    Add condition on the number of monuments left

--- app/javascript/packs/validate.js
+++ app/javascript/packs/validate.js
@@ -4,40 +4,60 @@ function validateButtons() {
   const card = document.querySelector(".card-test");
   let compteur = 0;
   let monument = itineraire.monuments[compteur];
+  // TO-DO: Replace the h1 by the true card
   card.insertAdjacentHTML("beforeend", `<h1>${monument.name}</h1>`);
 
   const yesButton = document.querySelector('.validate-yes');
   const noButton = document.querySelector('.validate-no');
 
   yesButton.addEventListener("click", (event) => {
+    // Generate the POST request to add the monuments to the itineraire
     Rails.ajax({
       type: "POST",
       url: `/vos-monuments/${itineraire.id}/${itineraire.monuments[compteur].id}`,
       data: 'first_name=Ricky&last_name=Bobby',
-      success: function(data) {
-        console.log("AAAAAAAA");
-      },
-      error: function() { console.log("Shit"); }
+      success: function() { console.log("Réussi boy!"); },
+      error: function() { console.log("Shit!"); }
     });
+
     // If no button is clicked, increase the compteur
     compteur += 1;
-    // Reassign the name
-    let monument = itineraire.monuments[compteur];
-    // Clear the HTML of the precedent name
-    card.innerHTML = "";
-    // Add the new name
-    card.insertAdjacentHTML("beforeend", `<h1>${monument.name}</h1>`);
+
+    // Test if there is stille monuments left to display
+    if (compteur > gon.itineraire.monuments.length - 1) {
+      // Clear the HTML of the precedent name
+      card.innerHTML = "";
+      // Display the fact that there is no more monuments
+      card.insertAdjacentHTML("beforeend", `<h1>No more Monuments !</h1>`);
+    } else {
+      // Reassign the name
+      let monument = itineraire.monuments[compteur];
+      // Clear the HTML of the precedent name
+      card.innerHTML = "";
+      // Add the new name
+      card.insertAdjacentHTML("beforeend", `<h1>${monument.name}</h1>`);
+    }
   });
 
   noButton.addEventListener("click", (event) => {
     // If no button is clicked, increase the compteur
     compteur += 1;
-    // Reassign the name
-    let monument = itineraire.monuments[compteur];
-    // Clear the HTML of the precedent name
-    card.innerHTML = "";
-    // Add the new name
-    card.insertAdjacentHTML("beforeend", `<h1>${monument.name}</h1>`);
+
+    // Test if there is stille monuments left to display
+    if (compteur > gon.itineraire.monuments.length - 1) {
+      // Clear the HTML of the precedent name
+      card.innerHTML = "";
+      // Display the fact that there is no more monuments
+      card.insertAdjacentHTML("beforeend", `<h1>No more Monuments !</h1>`);
+    } else {
+      // Reassign the name
+      let monument = itineraire.monuments[compteur];
+      // Clear the HTML of the precedent name
+      card.innerHTML = "";
+      // Add the new name
+      card.insertAdjacentHTML("beforeend", `<h1>${monument.name}</h1>`);
+    }
+
   });
 
 }
