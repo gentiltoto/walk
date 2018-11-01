7f14304810f8428c0a850d729f6b7ca09a8fd2d9
Henri Terrasse <henri.terrasse@edhec.com>
2018-11-01 12:53:45 +0100

    Interactivité map OK

--- app/javascript/lib/mapbox.js
+++ app/javascript/lib/mapbox.js
@@ -1,27 +1,67 @@
 //Fonction qui affiche la map dans la div "map".
+//Array d'array de coordonnée, pour centrer la carte comme il le faut.
+
 export function map(inp) {
   mapboxgl.accessToken = 'pk.eyJ1IjoiaGVucmk0NSIsImEiOiJjam52bjV4anAwYjc2M3ZxdHd5NjlpbGc5In0.jNKjBZ2d3T4G7qzcmRb77A'
-
   var map = new mapboxgl.Map({
-    container: 'map',
+    container: 'map-final',
+
+// center au milieu de la france, pour un petit effet par la suite
+    center: [2.918381, 47.026351],
     style: 'mapbox://styles/mapbox/basic-v9',
-    center: inp,
     zoom: 12
   });
 
+// Réduit inp en une seule géoloc puis centre la carte sur cette localisation
+  var bounds = inp.reduce(function(bounds, coord) {
+            return bounds.extend(coord);
+        }, new mapboxgl.LngLatBounds(inp[0], inp[0]));
+
+        map.fitBounds(bounds, {
+            padding: 50
+        });
+
+// Création de marker sous forme de div TO DO : retoucher aux marker pour les rendre plus sexy
+var miniFiche = '<div> TEST <div>'; // TO DO : implémentation de la récupération de la fiche
+inp.forEach((coord, index) => {
+  let el = document.createElement('div');
+  el.id = `marker${index}`;
+  el.classList.add("marker");
+  el.addEventListener("click", () => {
+  map = document.getElementById('map-final');
+  map.insertAdjacentHTML('afterend',miniFiche);
+})
+  new mapboxgl.Marker(el)
+    .setLngLat(coord)
+    .addTo(map);
+})
+
+
+
+
+// retourne une map, pour pouvoir l'utiliser sur un itinéraire par la suite
   return map;
 }
 
 
-//Fonction qui affiche un marker dans la map.
-export function marker(map, inp) {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiaGVucmk0NSIsImEiOiJjam52bjV4anAwYjc2M3ZxdHd5NjlpbGc5In0.jNKjBZ2d3T4G7qzcmRb77A'
-  // create a HTML element for each feature
-  var el = document.createElement('div');
-  el.className = 'marker';
+//Fonction qui affiche un marker dans la map. CETTE FONCTION A ETE IMPLEMENTE DANS MAP
+// export function marker(map, inp) {
+//   mapboxgl.accessToken = 'pk.eyJ1IjoiaGVucmk0NSIsImEiOiJjam52bjV4anAwYjc2M3ZxdHd5NjlpbGc5In0.jNKjBZ2d3T4G7qzcmRb77A'
+//   // create a HTML element for each feature
+//   var el = document.createElement('div');
+//   el.className = 'marker';
 
-  // make a marker for each feature and add to the map
-  new mapboxgl.Marker(el)
-    .setLngLat(inp)
-    .addTo(map);
-}
+//   // make a marker for each feature and add to the map
+//   new mapboxgl.Marker(el)
+//     .setLngLat(inp)
+//     .addTo(map);
+// }
+
+
+
+
+
+
+
+
+
--- app/javascript/packs/itinary-mapbox-direction.js
+++ app/javascript/packs/itinary-mapbox-direction.js
@@ -1,7 +1,8 @@
 import { getRoute } from "../lib/mapbox-direction"
 import { map } from "../lib/mapbox"
+// Coordoonéees rentrée en durs
 
-const mape = map([3.0565, 50.6302])
+const mape = map([[3.0565, 50.6302],[3.1565, 50.6302],[3.1565, 50.5302]]);
 mape.on('load', function() {
   getRoute(mape, [[3.0565, 50.6302],[3.1565, 50.6302],[3.1565, 50.5302]]);
 });
