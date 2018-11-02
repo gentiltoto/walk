9a6e90345edc0f998e0c29cc7c86ea121bfabbe6
AntoineTorrini <antoine.torrini@gmail.com>
2018-11-02 19:26:28 +0100

    correction for validate

--- app/javascript/packs/validate.js
+++ app/javascript/packs/validate.js
@@ -1,23 +1,16 @@
-function validateButtons() {
-  // Get the gon variable
-  const itineraire = gon.itineraire
+function modifyDOM(monument) {
   // Get the card of monument
-  const card = document.querySelector(".container-texte");
-
-  // Get the no-more card
-  const noMore = document.querySelector(".no-more");
-  // Get the link of the card
-  const linkFetch = document.querySelector(".link-fetch")
+  let card = document.querySelector(".container-texte");
   // Get the picture on the front
-  const picFront = document.querySelector("#flip-card-front")
+  let picFront = document.querySelector("#flip-card-front");
   // Get the map in the back
-  const mapBack = document.querySelector("#flip-card-back")
+  let mapBack = document.querySelector("#flip-card-back");
 
-  // Initialize the compteur
-  let compteur = gon.itineraire.compteur;
-  let monument = gon.monuments[compteur];
+  // Clear the HTML of the precedent name
+  card.innerHTML = "";
+  picFront.innerHTML = "";
+  mapBack.innerHTML = "";
 
-  // TO-DO: Replace the h1 by the true card
   card.insertAdjacentHTML("beforeend", `
   <h2>${monument.name}</h2>
   <p>Stars Yelp</p>
@@ -28,16 +21,39 @@ function validateButtons() {
   <p>Super j'ai adoré</p>
   <p>magnifiik surtout quand il fait beau</p>
   `);
+
   picFront.insertAdjacentHTML("beforeend", `
   <img src="${monument.photo.url}" alt="monument" class="monument-image"><i class="fas fa-map-marked-alt" id="flip-card-button"></i>
   `);
+
   mapBack.insertAdjacentHTML("beforeend", `<img src="https://api.mapbox.com/styles/v1/mapbox/light-v9/static/pin-s-marker+285A98(${monument.longitude},${monument.latitude})/${monument.longitude},${monument.latitude},11,0,0/360x220@2x?access_token=pk.eyJ1IjoiaGVucmk0NSIsImEiOiJjam52bjV4anAwYjc2M3ZxdHd5NjlpbGc5In0.jNKjBZ2d3T4G7qzcmRb77A" alt="monument" class= "map-image"><i class="fas fa-image" id="flip-card-button"></i>`);
+}
+
+
+
+function validateButtons() {
+  // Get the gon variable
+  const itineraire = gon.itineraire
+  
+  // Get the no-more card
+  const noMore = document.querySelector(".no-more");
+  // Get the link of the card
+  const linkFetch = document.querySelector(".link-fetch")
+  
+
+  // Initialize the compteur
+  let compteur = gon.itineraire.compteur;
+  let monument = gon.monuments[compteur];
+
+  modifyDOM(monument);
 
   const yesButton = document.querySelector('.validate-yes');
   const noButton = document.querySelector('.validate-no');
 
   yesButton.addEventListener("click", (event) => {
     // Generate the POST request to add the monuments to the itineraire
+    console.log(itineraire);
+    console.log(monument);
     Rails.ajax({
       type: "POST",
       url: `/vos-monuments/${itineraire.id}/${monument.id}`,
@@ -51,6 +67,7 @@ function validateButtons() {
 
     // Test if there is still monuments left to display
     if (compteur > gon.monuments.length - 1) {
+      let card = document.querySelector(".container-texte");
       // Clear the HTML of the precedent name
       card.innerHTML = "";
       // Remove the card
@@ -59,28 +76,9 @@ function validateButtons() {
       noMore.style.display = "block";
     } else {
       // Reassign the name
-      let monument = gon.monuments[compteur];
-      // Clear the HTML of the precedent name
-      card.innerHTML = "";
-      // Add the new name
-      card.insertAdjacentHTML("beforeend", `
-      <h2>${monument.name}</h2>
-      <p>Stars Yelp</p>
-      <p>${monument.description}</p>
-      <h3>Horaires d'ouverture</h3>
-      <p></p>
-      <h3>Avis</h3>
-      <p>Super j'ai adoré</p>
-      <p>magnifiik surtout quand il fait beau</p>
-      `);
-      // Set the new map
-      mapBack.innerHTML = "";
-      mapBack.insertAdjacentHTML("beforeend", `<img src="https://api.mapbox.com/styles/v1/mapbox/light-v9/static/pin-s-marker+285A98(${monument.longitude},${monument.latitude})/${monument.longitude},${monument.latitude},11,0,0/360x220@2x?access_token=pk.eyJ1IjoiaGVucmk0NSIsImEiOiJjam52bjV4anAwYjc2M3ZxdHd5NjlpbGc5In0.jNKjBZ2d3T4G7qzcmRb77A" alt="monument" class= "map-image"><i class="fas fa-image" id="flip-card-button"></i>`);
+      monument = gon.monuments[compteur];
 
-      picFront.innerHTML = "";
-      picFront.insertAdjacentHTML("beforeend", `
-      <img src="${monument.photo.url}" alt="monument" class="monument-image"><i class="fas fa-map-marked-alt" id="flip-card-button"></i>
-      `);
+      modifyDOM(monument);
     }
   });
 
@@ -90,6 +88,7 @@ function validateButtons() {
 
     // Test if there is still monuments left to display
     if (compteur > gon.monuments.length - 1) {
+      let card = document.querySelector(".container-texte");
       // Clear the HTML of the precedent name
       card.innerHTML = "";
       // Remove the card
@@ -98,28 +97,9 @@ function validateButtons() {
       noMore.style.display = "block";
     } else {
       // Reassign the name
-      let monument = gon.monuments[compteur];
-      // Clear the HTML of the precedent name
-      card.innerHTML = "";
-      // Add the new name
-      card.insertAdjacentHTML("beforeend", `
-      <h2>${monument.name}</h2>
-      <p>Stars Yelp</p>
-      <p>${monument.description}</p>
-      <h3>Horaires d'ouverture</h3>
-      <p></p>
-      <h3>Avis</h3>
-      <p>Super j'ai adoré</p>
-      <p>magnifiik surtout quand il fait beau</p>
-      `);
-      // Set the new map
-      mapBack.innerHTML = "";
-      mapBack.insertAdjacentHTML("beforeend", `<img src="https://api.mapbox.com/styles/v1/mapbox/light-v9/static/pin-s-marker+285A98(${monument.longitude},${monument.latitude})/${monument.longitude},${monument.latitude},11,0,0/360x220@2x?access_token=pk.eyJ1IjoiaGVucmk0NSIsImEiOiJjam52bjV4anAwYjc2M3ZxdHd5NjlpbGc5In0.jNKjBZ2d3T4G7qzcmRb77A" alt="monument" class= "map-image"><i class="fas fa-image" id="flip-card-button"></i>`);
-
-      picFront.innerHTML = "";
-      picFront.insertAdjacentHTML("beforeend", `
-      <img src="${monument.photo.url}" alt="monument" class="monument-image"><i class="fas fa-map-marked-alt" id="flip-card-button"></i>
-      `);
+      monument = gon.monuments[compteur];
+      
+      modifyDOM(monument);
     }
   });
 
