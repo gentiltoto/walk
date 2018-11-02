b1a09ec97bade306776c66e543ddd6c9cf1ccf58
Henri Terrasse <henri.terrasse@edhec.com>
2018-11-02 12:48:40 +0100

    JS poiur la carte

--- app/javascript/lib/mapbox.js
+++ app/javascript/lib/mapbox.js
@@ -6,41 +6,71 @@ export function map(inp) {
   var map = new mapboxgl.Map({
     container: 'map-final',
 
-// center au milieu de la france, pour un petit effet par la suite
+  // center au milieu de la france, pour un petit effet par la suite
     center: [2.918381, 47.026351],
     style: 'mapbox://styles/mapbox/basic-v9',
     zoom: 12
   });
 
-// Réduit inp en une seule géoloc puis centre la carte sur cette localisation
+  // Réduit inp en une seule géoloc puis centre la carte sur cette localisation
   var bounds = inp.reduce(function(bounds, coord) {
-            return bounds.extend(coord);
-        }, new mapboxgl.LngLatBounds(inp[0], inp[0]));
+    return bounds.extend(coord);
+    }, new mapboxgl.LngLatBounds(inp[0], inp[0]));
 
-        map.fitBounds(bounds, {
-            padding: 50
-        });
+  map.fitBounds(bounds, {
+      padding: 50
+  });
 
-// Création de marker sous forme de div TO DO : retoucher aux marker pour les rendre plus sexy
-var miniFiche = '<div> TEST <div>'; // TO DO : implémentation de la récupération de la fiche
-inp.forEach((coord, index) => {
-  let el = document.createElement('div');
-  el.id = `marker${index}`;
-  el.classList.add("marker");
-  el.addEventListener("click", () => {
-  map = document.getElementById('map-final');
-  map.insertAdjacentHTML('afterend',miniFiche);
-})
-  new mapboxgl.Marker(el)
-    .setLngLat(coord)
-    .addTo(map);
-})
+// Création de marker sous forme de div      TO DO : retoucher aux marker pour les rendre plus sexy
 
+  inp.forEach((coord, index) => {
+    let el = document.createElement('div');
+    el.id = `marker-${index}`; // L'ID du marker devra être celui du monument dans la DATABASE
+    el.classList.add("marker");
+    new mapboxgl.Marker(el)
+      .setLngLat(coord)
+      .addTo(map)
+  })
 
+  return map
+}
 
+// Fonction qui affiche un recap lorsqu'on clic sur des markers. id = array des ID des monuments dans la DB affichés sur la map
+export function eventListener(ids) {
+// Mise en place des events listener sur les recaps
+  ids.forEach((id) => {
+    document.getElementById(`marker-${id}`).addEventListener("click", () => {
+      document.getElementById(`recap-monument-${id}`).style.bottom = "0px";
+    });
+  });
+// Faire rentrer tous les recap lorsqu'on clic sur la croix (= repositionner tous les recap à - 1000)
+  document.querySelectorAll(".close-under-map").forEach((e) => {
+    e.addEventListener("click", () => {
+      document.querySelectorAll(".under-map").forEach((e) => {
+        e.style.bottom = "-1000px";
+      });
+    });
+  });
 
-// retourne une map, pour pouvoir l'utiliser sur un itinéraire par la suite
-  return map;
+// Affiche la fiche établissement lorsque le bouton dans le recap est utilisé
+  // Mise en place des events listener sur les boutons fiche
+  ids.forEach((id) => {
+    document.getElementById(`btn-fiche-${id}`).addEventListener("click", () => {
+      document.getElementById(`fiche-monument-${id}`).style.bottom = "0px";
+    });
+  });
+
+// Rabaisse toutes les fiches
+  document.querySelectorAll(".close-fiche-monument").forEach((e) => {
+    e.addEventListener("click", () => {
+      document.querySelectorAll(".fiche-final").forEach((e) => {
+        e.style.bottom = "-1000px";
+      })
+      document.querySelectorAll(".under-map").forEach((e) => {
+        e.style.bottom = "-1000px";
+      })
+    });
+  });
 }
 
 
@@ -56,8 +86,6 @@ inp.forEach((coord, index) => {
 //     .setLngLat(inp)
 //     .addTo(map);
 // }
-
-
 
 
 
--- app/javascript/packs/itinary-mapbox-direction.js
+++ app/javascript/packs/itinary-mapbox-direction.js
@@ -1,8 +1,11 @@
 import { getRoute } from "../lib/mapbox-direction"
-import { map } from "../lib/mapbox"
+import { map, eventListener } from "../lib/mapbox"
 // Coordoonéees rentrée en durs
 
 const mape = map([[3.0565, 50.6302],[3.1565, 50.6302],[3.1565, 50.5302]]);
 mape.on('load', function() {
   getRoute(mape, [[3.0565, 50.6302],[3.1565, 50.6302],[3.1565, 50.5302]]);
 });
+// argument = ID des monuments dans la data base.
+eventListener([0,1]);
+
