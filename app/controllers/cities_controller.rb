b7a7b54f5ccf186a32fd1d1283058e6c30c765e9
AntoineTorrini <antoine.torrini@gmail.com>
2018-11-04 11:40:01 +0100

    Add sweet transition and map marker changes

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,7 +2,8 @@ class CitiesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :components]
 
   def components
-    @monument = Monument.find(125)
+    @monuments = Monument.where(city: "1")
+    gon.monuments = @monuments
   end
 
   def home
--- app/javascript/packs/components.js
+++ app/javascript/packs/components.js
@@ -3,37 +3,88 @@ function map(inp, offset0, offset1) {
   var map = new mapboxgl.Map({
     container: 'map',
 
-    center: [inp[0][0] - offset0, inp[0][1] - offset1],
+    center: [inp[0] - offset0, inp[1] - offset1],
     style: 'mapbox://styles/mapbox/light-v9',
     zoom: 15
   });
 
 // // Création de marker sous forme de div      TO DO : retoucher aux marker pour les rendre plus sexy
-//
-  inp.forEach((coord, index) => {
-    let el = document.createElement('div');
-    el.id = `marker-${index}`; // L'ID du marker devra être celui du monument dans la DATABASE
-    el.classList.add("marker");
-    new mapboxgl.Marker(el)
-      .setLngLat(coord)
-      .addTo(map)
-  })
-
   return map
 }
 
-let mapObject;
-
-if ($(window).width() < 992) {
-  mapObject = map([[3.063333, 50.637222]], 0, 0.002);
-} else {
-  mapObject = map([[3.063333, 50.637222]], 0.007, 0);
+function addMarker(inp, map) {
+  let el = document.createElement('div');
+  el.id = `marker-0`;
+  el.classList.add("marker");
+  let marker = new mapboxgl.Marker(el)
+    .setLngLat(inp)
+    .addTo(map)
+  return marker
 }
 
+function appendToDOM(nb) {
+  const imageDiv = $(".monument-image-div");
+  const nameDiv = $(".monument-name");
+
+  imageDiv.html("");
+  nameDiv.html("");
+  imageDiv.append(`<img class="monument-image-new" src="${gonMonuments[nb].photo.url}">`);
+  nameDiv.append(`${gonMonuments[nb].name}`)
+}
+
+function formatCoord(nb) {
+  let coord = [parseFloat(gonMonuments[nb].longitude), parseFloat(gonMonuments[nb].latitude)];
+  return coord
+}
+
+function defineNewMarker(nb, map, marker) {
+  markerObject.remove()
+  markerObject = addMarker(formatCoord(nb), mapObject);
+  let flyToObject;
+  if ($(window).width() < 992) {
+    flyToObject = [formatCoord(nb)[0], formatCoord(nb)[1] - 0.002];
+  } else {
+    flyToObject = [formatCoord(nb)[0] - 0.007, formatCoord(nb)[1]];
+  }
+  mapObject.flyTo({
+        center: flyToObject
+    });
+}
+
+let mapObject;
+let markerObject;
+let compteur = 0;
+const gonMonuments = gon.monuments;
+
+if ($(window).width() < 992) {
+  mapObject = map(formatCoord(compteur), 0, 0.002);
+  markerObject = addMarker(formatCoord(compteur), mapObject);
+} else {
+  mapObject = map(formatCoord(compteur), 0.007, 0);
+  markerObject = addMarker(formatCoord(compteur), mapObject);
+}
+
+// Validate buttons handlers
+const choiceNo = $(".choice-no").click((event) => {
+  compteur += 1;
+  appendToDOM(compteur);
+  defineNewMarker(compteur, mapObject, markerObject);
+});
+const choiceYes = $(".choice-yes").click((event) => {
+  console.log("POST");
+  compteur += 1;
+  appendToDOM(compteur);
+  defineNewMarker(compteur, mapObject, markerObject);
+});
+
+
+// Resize handler
 $(window).resize(function(event) {
   if ($(window).width() < 992) {
-    mapObject = map([[3.063333, 50.637222]], 0, 0.002);
+    mapObject = map(formatCoord(compteur), 0, 0.002);
+    markerObject = addMarker(formatCoord(compteur), mapObject);
   } else {
-    mapObject = map([[3.063333, 50.637222]], 0.007, 0);
+    mapObject = map(formatCoord(compteur), 0.007, 0);
+    markerObject = addMarker(formatCoord(compteur), mapObject);
   }
 });
