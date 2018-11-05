e9c01f7c51d32d859740604732bae8ab280866c1
cassandre jacson <cassandre.jacson@edhec.com>
2018-11-05 09:14:27 +0100

    explics en cours

---
+++ app/javascript/packs/explications.js
@@ -0,0 +1,13 @@
+// function map(inp, offset0, offset1) {
+//   mapboxgl.accessToken = 'pk.eyJ1IjoiaGVucmk0NSIsImEiOiJjam52bjV4anAwYjc2M3ZxdHd5NjlpbGc5In0.jNKjBZ2d3T4G7qzcmRb77A'
+//   var map = new mapboxgl.Map({
+//     container: 'map',
+
+//     center: [inp[0] - offset0, inp[1] - offset1],
+//     style: 'mapbox://styles/mapbox/light-v9',
+//     zoom: 15
+//   });
+
+// // // Création de marker sous forme de div      TO DO : retoucher aux marker pour les rendre plus sexy
+//   return map
+//}
