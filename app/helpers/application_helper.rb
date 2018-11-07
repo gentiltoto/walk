7cd534623610168eebabb8ef464d46c44b1f8b8d
Henri Terrasse <henri.terrasse@edhec.com>
2018-11-07 16:20:02 +0100

    Stars OK

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def score_to_stars(score)
+    case score.to_i
+    when 200 .. 1000
+      return 5
+    when 150 .. 200
+      return 4.5
+    when 100 .. 150
+      return 4.0
+    when 75 .. 100
+      return 3.5
+    when 50 .. 75
+      return 3
+    when 25 .. 50
+      return 2.5
+    else
+      return 2
+    end
+  end
 end
