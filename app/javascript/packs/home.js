9cee6394bcf06dd8fa530d287e9b8807ac048170
cassandre jacson <cassandre.jacson@edhec.com>
2018-11-08 10:39:03 +0100

    sign in done

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -3,8 +3,8 @@ import Typed from 'typed.js';
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
     strings: ["Crée ton itinéraire en un clic !"],
-    typeSpeed: 60,
-    loop: true
+    typeSpeed: 70,
+    loop: false
   });
 }
 
