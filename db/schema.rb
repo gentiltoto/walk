55af8fba338e7816104bc784aba889fa96bab993
Henri Terrasse <henri.terrasse@edhec.com>
2018-11-08 13:20:28 +0100

    Migration metrics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_153348) do
+ActiveRecord::Schema.define(version: 2018_11_08_094801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_11_07_153348) do
     t.datetime "updated_at", null: false
     t.bigint "city_id"
     t.integer "compteur", default: 0
+    t.string "duration"
     t.index ["city_id"], name: "index_itineraires_on_city_id"
     t.index ["user_id"], name: "index_itineraires_on_user_id"
   end
